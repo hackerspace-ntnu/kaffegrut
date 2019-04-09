d83748a205fc0fefa1a9b2cfa0089c34a6ac21a6
Torstein Nordgård-Hansen <torsteinnh@gmail.com>
2019-04-09 20:38:52 +0200

    Did some fixing in order to no longer depend on systemclock

--- ArduinoCode/kaffe/kaffe.ino
+++ ArduinoCode/kaffe/kaffe.ino
@@ -1,22 +1,15 @@
-
 /*
 	FILNAVN: kaffe.ino
 	FORMÅL: Hovedlogikk som tracker kaffekanners kvalitet.
 	ORIGINALFORFATTER: Torstein Nordgård-Hansen
 	PÅBEGYNT: 2018
 	REQUIREMENTS: Arduino-mikrokontroller
-	NAVNGIVNING:
-	 * Skal baseres på suffix etter følgende kriterier:
-	 * Dersom en kaffekanne blir knust eller på annet vis må erstattes,
-	 * skal den følge samme navngivning som tidligere med tall etter romersk
-	 * tallsystem som indikerer generasjonen.
-	 * Eksempel: Finn III blir på tragisk vis skutt av en politi-robot i
-	 * fremtidens dystopiske samfunn, så da blir nye kannen døpt Finn IV. 
 	ENDRINGSLOGG: https://github.com/hackerspace-ntnu/kaffegrut
  */
 
 // Inkluder LCD-bibliotek
 #include <LiquidCrystal.h>
+
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
@@ -24,15 +17,22 @@ const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 
-int finn = 0;
-int matias = 0;
+int lastTime = 0;
+
+
+int finnjr = 0;
+int matiasito = 0;
+
+bool finnjrValid = false;
+bool matiasitoValid = false;
+
 
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
 
   // Print a message to the LCD.
-  lcd.print("Finn jr   :");
+  lcd.print("Finnjr jr   :");
   lcd.setCursor(0, 1);
   lcd.print("Mathiasito:");
 
@@ -41,26 +41,62 @@ void setup() {
   pinMode(8, INPUT);
 }
 
+
 void loop() {
-  getUpdate(&finn, &matias);
-  
+  if (digitalRead(7)){ getUpdate(&finnjr, &finnjrValid); }
+  if (digitalRead(8)){ getUpdate(&matiasito, &matiasitoValid); }
+
+  if (isTime()){
+    if (finnjrValid){
+      finnjr += 1;
+    }
+    if (matiasitoValid){
+      matiasito += 1;
+    }
+  }
+
   lcd.setCursor(11, 0);
-  lcd.print(hrTime((millis()/1000) , finn));
+  lcd.print(hrTime(finnjr, &finnjrValid));
 
   lcd.setCursor(11, 1);
-	lcd.print(hrTime((millis()/1000) , matias));
+  lcd.print(hrTime(matiasito, &matiasitoValid));
 }
 
-String hrTime(int seconds, int kanne){
-  int h = (seconds - kanne)/3600;
-  int m = ((seconds - kanne)/60)%60;
-	int s = (seconds - kanne)%60;
+String hrTime(int kanne, bool *valid){
+  int h = kanne/3600;
+  int m = (kanne/60)%60;
+  int s = kanne%60;
+
+  if ( h >= 8 ){
+    *valid = false;
+  }
   
-	return ( (kanne == 0 || (seconds - kanne < 0)) ? " N.A." : ( (h >= 5 ? "  old" :  ( h > 9 ? (String)(h): "0"+(String)(h)) + ":" + ( m > 9 ? (String)(m): "0"+(String)(m)))));
+  if ( *valid ){
+    return (( h > 9 ? (String)(h): "0"+(String)(h)) + ":" + ( m > 9 ? (String)(m): "0"+(String)(m)));
+  }
+  else if ( kanne == 0 ){
+    return ( "N.A.");
+  }
+  else {
+    return ("8+h ");
+  }
 }
 
-void getUpdate(int *finn, int *matias){
-  if (digitalRead(7)){ *finn = millis()/1000;}
-  if (digitalRead(8)){ *matias = millis()/1000;}
+void getUpdate(int* kannetid, bool* kanneValid){
+  *kannetid = millis()/1000;
+  *kanneValid = true;
 }
 
+bool isTime(){
+  if (millis() > 1000 + lastTime && millis() < 100000 + lastTime){
+    lastTime = millis();
+    return true;
+  }
+  else if (millis() < lastTime){
+    lastTime = 0;
+    return true;
+  }
+  else {
+    return false;
+  }
+}
--- README.md
+++ README.md
@@ -7,12 +7,3 @@ Krever:
 - Arduino
 - LiquidCrystal.h
 
-
-## Navngivning
-
-Navngivning av kaffekanner skal baseres på suffix etter følgende kriterier:
-
-* Dersom en kaffekanne blir knust eller på annet vis må erstattes, skal den følge samme navngivning som tidligere med tall etter romersk tallsystem som indikerer generasjonen på den nye kannen. 
-Eksempel: Finn III blir på tragisk vis skutt av en politi-robot i fremtidens dystopiske samfunn, så da blir nye kannen døpt Finn IV.
-
-* Dersom det oppstår en sitajson hvor to kanner har samme navn, skal den yngste kannen få suffix "Jr". Dersom det er hensiktsmessig, kan også "Sr." brukes for den eldre.
